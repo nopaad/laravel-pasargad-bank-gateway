767e9c773a6c3e6fc856400e5a34a5202b8513f4
Aria <aria@radmand.net>
2016-12-26 13:34:29 +0330

    gateway

--- config/laravel-pasargad-bank.php
+++ config/laravel-pasargad-bank.php
@@ -9,4 +9,5 @@ return [
 	'merchant' => env('PASARGAD_MERCHANT_CODE'),
 	'terminal' => env('PASARGAD_TERMINAL_ID'),
 	'private_key' => env('PASARGAD_PRIVATE_KEY'),
+	'url' => env('PASARGAD_URL', 'https://pep.shaparak.ir/gateway.aspx'),
 ];
--- src/Gateway.php
+++ src/Gateway.php
@@ -3,21 +3,23 @@ namespace Nopaad\PasargadBank;
 class Gateway
 {
 	public static function sign($amount, $redirect, $invoice_number, $action){
-		$plaintext =
-			'#' . config('laravel-pasargad-bank.merchant') .
-			'#' . config('laravel-pasargad-bank.terminal') .
-			'#' . $invoice_number .
-			'#' . date("Y-m-d H:i:s") .
-			'#' . $amount .
-			'#' . $redirect .
-			'#' . $action .
-			'#' . date("Y-m-d H:i:s") .
-			'#';
+		$output = $input = [
+			'merchantCode' => config('laravel-pasargad-bank.merchant'),
+			'terminalCode' => config('laravel-pasargad-bank.terminal'),
+			'invoiceNumber' => $invoice_number,
+			'invoiceDate' => date("Y-m-d H:i:s"),
+			'amount' => $amount,
+			'redirectAddress' => $redirect,
+			'action' => $action,
+			'timeStamp' => date("Y-m-d H:i:s"),
+		];
+		$plaintext = '#'. collect($input)->implode('#') . '#';
 		$rsa = new \phpseclib\Crypt\RSA();
 		$rsa->loadKey(config('laravel-pasargad-bank.private_key')); // private key
 		$rsa->setSignatureMode(\phpseclib\Crypt\RSA::SIGNATURE_PKCS1);
-		$signature = $rsa->sign($plaintext);
-		$signature = base64_encode($signature);
-		return $signature;
+		$output['sign'] = $rsa->sign($plaintext);
+		$output['sign'] = base64_encode($output['sign']);
+
+		return collect($output);
 	}
 }
